677833f4be3d6dd6560621fa54b95b073d8b8c47
dhannjay raj sankhla <dhannjayrajsankhla@gmail.com>
2026-01-21 17:33:01 +0530

    add producr and category in data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { ENV } from './config/env';
 import { User } from './entity/User';
+import { Product } from './entity/Product';
+import { Category } from './entity/Category';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -11,6 +13,6 @@ export const AppDataSource = new DataSource({
   password: ENV.DB_PASS,
   database: ENV.DB_NAME,
   synchronize: true, // dev only
-  logging: false,
-  entities: [User],
+  logging: true,
+  entities: [User,Product,Category],
 });
